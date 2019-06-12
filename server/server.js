c65decac73ac07fcc90e17e56762311a4dd6f42f
MitziYolotzin <ultravisuell@hotmail.com>
2019-06-11 20:08:38 -0500

    create server with express, create listen port

--- server/server.js
+++ server/server.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
-//const port = process.env.PORT || 
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+    console.log('Server Runing ')
+})
