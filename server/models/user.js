1f487790ad6e7923805ec3f8ac8f8ee7c46527c2
MitziYolotzin <ultravisuell@hotmail.com>
2019-06-16 11:02:22 -0500

    login user routes, check password compare db

--- server/models/user.js
+++ server/models/user.js
@@ -1,94 +1,88 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const SALT_I = 10;
-require('dotenv').config();
+require("dotenv").config();
 
 const userSchema = mongoose.Schema({
-email:{
+  email: {
     type: String,
     required: true,
     trim: true,
     unique: 1
-},
-password:{
+  },
+  password: {
     type: String,
     required: true,
     minlenght: 5
-},
-name:{
+  },
+  name: {
     type: String,
     required: true,
     maxlenght: 100
-},
-lastname:{
+  },
+  lastname: {
     type: String,
     required: true,
     maxlenght: 100
-},
-cart: {
+  },
+  cart: {
     type: Array,
     default: []
-},
-history:{
+  },
+  history: {
     type: Array,
     default: []
-},
-role:{
-    type:Number,
+  },
+  role: {
+    type: Number,
     default: 0
-},
-token:{
-    type:String
-}
-
+  },
+  token: {
+    type: String
+  }
 });
 
-userSchema.pre('save', function(next) {
-var user = this;
-
-if(user.isModified('password')){
-    bcrypt.genSalt(SALT_I, function(err,salt) {
-        if(err) return next(err);
-    
-        bcrypt.hash(user.password, salt, function(err, hash) {
-    if(err) return next(err);
-    user.password = hash;
+userSchema.pre("save", function(next) {
+  var user = this;
+  //In case user is modified, again encrypt
+  if (user.isModified("password")) {
+    bcrypt.genSalt(SALT_I, function(err, salt) {
+      if (err) return next(err);
+      //hash transform ny block of arbitrary data into new string of characters
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
     next();
-        });
-    })
-} else {
-    next() 
-}
+  }
+});
 
-})
+//cb connect with server user steps in app.post
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
+userSchema.methods.generateToken = function(cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), process.env.SECRET);
 
-userSchema.methods.comparePassword = function(candidatePassword, cb){
-bcrypt.compare(candidatePassword,this.password, function(err,isMatch){
- if(err) return cb(err);
- cb(null,isMatch)
-})
-
-}
-
-userSchema.methods.generateToken = function(){
-     var user = this;
-     var token = jwt.sign(user._id.teHexString(),process.env.SECRET)
-
-     //user.id + pass
-user.token = token;
-user.save(function(err,user){
-    if(err) return cb(err);
-    cb(null,user);
-})
-
-}
-
-
-
+  //user.id + pass
+  //pass on the enviroment
+  user.token = token;
+  user.save(function(err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 //MODEL-model, esquema, coleccion donde se guarda "users"
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 //EXPORT
-module.exports = { User }
+module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-const mongoose = require ('mongoose');
-require ('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 //CONECTION AND SELL
- mongoose.Promise = global.Promise;
- mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
-    if(err) return err
-    console.log("Conectado a MongoDB")
-});
-
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  process.env.DATABASE,
+  { useNewUrlParser: true, useCreateIndex: true },
+  err => {
+    if (err) return err;
+    console.log("Connected with MongoDB");
+  }
+);
 
 // mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true },  (err) => {
 //     if (err){
@@ -22,59 +24,60 @@ require ('dotenv').config();
 //     console.log("conectado a mongo")
 //   });
 
-
 //MIDDLEWARE
-app.use(bodyParser.urlencoded({useNewUrlParser: true, extended:true}));
- app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ useNewUrlParser: true, extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 
 //MODELS
-const { User } = require ('./models/user');
+const { User } = require("./models/user");
 
+//USERS, create new user
+app.post("/api/users/register", (req, res) => {
+  // res.status(200);
+  const user = new User(req.body);
 
-//USERS
-app.post('/api/users/register', (req, res) => {
-   // res.status(200);
-   const user = new User (req.body);
-
-   user.save((err, doc) => {
-       if(err) return res.json({success:false,err});
-       res.status(200).json({
-           success: true,
-           userdata: doc
-       })
-   })
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      userdata: doc
+    });
+  });
 });
 
-app.post('/api/users/login', (req, res) => {
-//find the email
-User.findOne({'email':req.body.email}, (err,user) => {
-    if(!user) return res.json({loginSuccess:false,message:'Auth failes, email not found'})
+//Send user and password
+app.post("/api/users/login", (req, res) => {
+  //find the email in DB, and user in DB
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found"
+      });
+//cb User schema comparePassword, passsword check with DB
+    //check pass, isMatch is true
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "Wrong password" });
 
-//check pass
-user.comparePassword(req.body.password, (err,isMatch)=> {
-if(!isMatch) return res.json({loginSuccess:false,message:"Wrong password"})
-
-//generate token
-user.generateToken((err,user) => {
-if(err) return res.status(400).send(err);
-res.cookie('w_auth',user.token).status(200).json({
-    loginSuccess: true
-})
-})
-
-})
-
-})
-
-
-})
-
-
+      //generate token
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("w_auth", user.token)  //store cookie token
+          .status(200)
+          .json({
+            loginSuccess: true
+          });
+      });
+    });
+  });
+});
 
 //PORT
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
-    console.log('Server Runing ')
-})
+  console.log("Server Running ");
+});
