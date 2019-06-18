3166b5157e03c37f874ccce18265d8a4d6e8c493
MitziYolotzin <ultravisuell@hotmail.com>
2019-06-17 19:20:19 -0500

    create cliente, react app, proxy localhost same port, and npm concurrently for server and cliente execute

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+
+  
+  //const [stateProduct, setStateProduct] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/product/brands').then(response => {
+      console.log(response)  }) 
+  }, []);
+  
+  // componentDidMount(){
+  //   axios.get('/api/product/brands').then(response => {
+  //     console.log(response)
+  //   })
+  // }
+
+  return (
+    <div className="App">
+      MY
+    </div>
+  );
+
+  };
+export default App;
