b5dd1fd5f2c0b126831c4778f2bbb3d8f3380e67
MitziYolotzin <ultravisuell@hotmail.com>
2019-06-17 22:07:20 -0500

    redux implement start, and create folder-files

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,21 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-
-  
   //const [stateProduct, setStateProduct] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/product/brands').then(response => {
-      console.log(response)  }) 
+    axios.get("/api/product/brands").then(response => {
+      console.log(response);
+    });
   }, []);
-  
+
   // componentDidMount(){
   //   axios.get('/api/product/brands').then(response => {
   //     console.log(response)
   //   })
   // }
 
-  return (
-    <div className="App">
-      MY
-    </div>
-  );
-
-  };
+  return <div className="App">MY</div>;
+};
 export default App;
